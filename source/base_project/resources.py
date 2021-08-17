9267661deb56d5b680f319b95ba32082b02fc34d
Denis <denisbolba@gmail.com>
2021-08-17 18:35:56 +0300

    removed mist

--- source/base_project/resources.py
+++ source/base_project/resources.py
@@ -7,7 +7,7 @@ def get_joke():
     value = randint(0, 6)
     type_of_joke = ["Mist", "Programming", "Dark", "Pun", "Spooky", "Christmas"]
 
-    url = "https://sv443.net/jokeapi/v2/joke/Misc/" + type_of_joke[value]
+    url = "https://sv443.net/jokeapi/v2/joke/" + type_of_joke[value]
 
     querystring = {"format": "txt", "contains": "", "idRange": "0-150", "blacklistFlags": "racist"}
 
